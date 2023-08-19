f8d33b9412c51735468e482724fed883ba824c2d
Lavinia Bianca <chelaru_laviniabianca@yahoo.com>
2023-08-19 17:37:08 +0200

    Add winning listener

--- script.js
+++ script.js
@@ -23,17 +23,30 @@ const Gameboard = () => {
         gameboard[row][column].addMarker(playerMarker);
 
     }
+    const getBoardValueAt=(row,column)=>{
+        return gameboard[row][column].getValue();
+    }
+    const reset=()=>{
+        for (let i = 0; i < rows; i++) {
+            gameboard[i] = [];
+            for (let j = 0; j < columns; j++) {
+                gameboard[i].push("");
+            }
+        }
+    }
+
     const printGameboard = () => {
         const boardWithCellValues =
             gameboard.map((row) => row.map((cell) => cell.getValue()))
         console.log(boardWithCellValues);
     }
     const getGameboard = () => gameboard;
-
-    return {
+    
+    return {reset,
         getGameboard,
         updateGameboard,
-        printGameboard
+        printGameboard,
+        getBoardValueAt
     }
 }
 const Cell = () => {
@@ -60,6 +73,17 @@ const Player = (name, marker) => {
     return { getName, getMarker }
 }
 const GameController = (playerOne, playerTwo) => {
+    const WINNING_COMBINATIONS =[
+        ['00','01','02'],
+        ['10','11','12'],
+        ['20','21','22'],
+        ['00','10','20'],
+        ['01','11','21'],
+        ['02','12','22'],
+        ['00','11','22'],
+        ['20','11','02']
+    ]
+
 
     const players = [playerOne, playerTwo];
     let activePlayer = players[0];
@@ -83,9 +107,29 @@ const GameController = (playerOne, playerTwo) => {
     const playRound = ([row, column]) => {
         console.log(`${getActivePlayer().getName()} put an ${getActivePlayer().getMarker()} on position ${[row, column]}`);
         board.updateGameboard(row, column, getActivePlayer().getMarker());
-    
+        if(checkWin(getActivePlayer().getMarker()))
+            return;
+        checkDraw();
+        
         switchPlayerTurn();
         printNewRound();
+    }
+    const checkWin=(marker)=>{
+        if(WINNING_COMBINATIONS.some(combination=>{
+            console.log(combination)
+            return combination.every(index=>{
+                return board.getBoardValueAt(index.charAt(0),index.charAt(1))==marker;
+            })
+        })){
+            console.log(`${getActivePlayer().getName()} won`);
+            return true;
+        }else{
+            return false;
+        }
+
+    }
+    const checkDraw=()=>{
+
     }
 
     printNewRound();
